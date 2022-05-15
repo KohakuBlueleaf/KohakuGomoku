76e6ec068733f5722895788aa50ad9ecea961f67
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2022-05-15 17:08:25 +0800

    Add alphabeta policy

---
+++ src/policy/alphabeta.cpp
@@ -0,0 +1,56 @@
+#include <tuple>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include "alphabeta.hpp"
+
+template<class Iter>
+inline size_t argmax(Iter first, Iter last){
+  return std::distance(first, std::max_element(first, last));
+}
+
+int AlphaBeta::eval(State *state, int depth, int alpha, int beta){
+  GAME_STATE now_res = state->check_res();
+  if(now_res == LOSE){
+    delete state;
+    return MINF;
+  }
+  if(now_res==DRAW){
+    delete state;
+    return 0;
+  }
+  if(depth==0){
+    int score = state->eval();
+    delete state;
+    return score;
+  }
+  
+  for(auto move: state->legal_actions){
+    //負極大值
+    //使用負極大值算法時，alpha_next = -beta, beta_next = -alpha
+    int score = -eval(&(state->next_state(move)), depth-1, -beta, -alpha);
+    alpha = max(score, alpha);
+    if(alpha >= beta){
+      delete state;
+      return alpha;
+    }
+  }
+  delete state;
+  return alpha;
+};
+
+Point AlphaBeta::get_move(State *state, int depth){
+  Point best_action = Point(-1, -1);
+  int alpha = MINF;
+
+  auto all_moves = state->legal_actions;
+  for(Point move: all_moves){
+    int score = -eval(&(state->next_state(move)), depth-1, MINF, -alpha);
+    if(score > alpha){
+      best_action = move;
+      alpha = score;
+    }
+  }
+
+  return best_action;
+};
---
+++ src/policy/alphabeta.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <tuple>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include "../state/state.hpp"
+
+#define max(a,b) ((a>b)?a:b)
+#define min(a,b) ((a<b)?a:b)
+
+#define MINF -2147483647
+#define INF 2147483647
+
+template<class Iter>
+inline size_t argmax(Iter first, Iter last);
+
+class AlphaBeta{
+  public:
+    static int eval(State *state, int depth, int alpha, int beta);
+    static Point get_move(State *state, int depth);
+};

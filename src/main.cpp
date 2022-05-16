dfdcd6cf55e0d51eedda8552536af66f7d3b5432
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2022-05-16 21:59:42 +0800

    Correct tabs to spaces

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,20 @@
 
 struct Point {
   int x, y;
-	Point() : Point(0, 0) {}
-	Point(float x, float y) : x(x), y(y) {}
-	bool operator==(const Point& rhs) const {
-		return x == rhs.x && y == rhs.y;
-	}
-	bool operator!=(const Point& rhs) const {
-		return !operator==(rhs);
-	}
-	Point operator+(const Point& rhs) const {
-		return Point(x + rhs.x, y + rhs.y);
-	}
-	Point operator-(const Point& rhs) const {
-		return Point(x - rhs.x, y - rhs.y);
-	}
+  Point() : Point(0, 0) {}
+  Point(float x, float y) : x(x), y(y) {}
+  bool operator==(const Point& rhs) const {
+    return x == rhs.x && y == rhs.y;
+  }
+  bool operator!=(const Point& rhs) const {
+    return !operator==(rhs);
+  }
+  Point operator+(const Point& rhs) const {
+    return Point(x + rhs.x, y + rhs.y);
+  }
+  Point operator-(const Point& rhs) const {
+    return Point(x - rhs.x, y - rhs.y);
+  }
 };
 
 class GomokuBoard {
